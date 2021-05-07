b2219d01e1058d488538ba8611dbe7a129e2bdeb
Pritom Karmokar <pritomkarmokar99@gmail.com>
2021-05-07 14:52:53 +0600

    simple problem

---
+++ codeforces/problem-746A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin>>a>>b>>c;
+
+    int sum = 0;
+
+    for(int i = a; i > 0 ; i--){
+        if(b >= 2*i && c >= 4*i){
+            sum = 7*i;
+            break;
+        }
+    }
+    cout<<sum<<endl;
+}
