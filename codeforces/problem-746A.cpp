15cfdda1e16ef6f620d4794b41ec30a02ab6da66
Pritom Karmokar <pritomkarmokar99@gmail.com>
2021-05-07 17:16:39 +0600

    solution of problem A Compote added

--- codeforces/problem-746A.cpp
+++ codeforces/problem-746A.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 int main()
 {
     int a, b, c;
     cin>>a>>b>>c;
-
+ 
     int sum = 0;
-
+ 
     for(int i = a; i > 0 ; i--){
         if(b >= 2*i && c >= 4*i){
             sum = 7*i;
