e882e6088c31ab61837d79c60c7d79b71cef6179
Pritom Karmokar <pritomkarmokar99@gmail.com>
2021-07-07 04:51:33 +0600

    solution of problem A Permutation Forgery added

---
+++ codeforces/problem-1405A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    
+    int arr[n];
+    for(int i = 0 ; i < n ; i++) cin>>arr[i];
+
+    for(int i = n-1 ; i >= 0 ; i--) cout<<arr[i]<<" ";
+    cout<<endl;
+
+}
+
+int main()
+{
+    int tc;
+    cin>>tc;
+
+    while(tc--){
+        solve();
+    }
+    return 0;
+}
