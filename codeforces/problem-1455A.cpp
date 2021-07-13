020b70af56c541b6b1851acb4917fdaec10a57f2
Pritom Karmokar <pritomkarmokar99@gmail.com>
2021-07-13 23:01:51 +0600

    solution of problem A Strange Functions added

---
+++ codeforces/problem-1455A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin>>tc;
+
+    while(tc--){
+        string l;
+        cin>>l;
+
+        int ans = l.size();
+
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
