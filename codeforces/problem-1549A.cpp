025e53446cc23a006c371fbe13dca0d6c296a222
Pritom Karmokar <pritomkarmokar99@gmail.com>
2021-10-27 00:55:42 +0600

    solution of problem A Gregor and Cryptography added

---
+++ codeforces/problem-1549A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+
+int main()
+{
+    //freopen("input.txt","r", stdin);
+    //freopen("output.txt","w", stdout);
+
+    int tc;
+    cin>>tc;
+
+    while(tc--){
+        int n;
+        cin>>n;
+        
+        cout<<2<<" "<<n-1<<endl;
+    }
+
+    return 0;
+}
